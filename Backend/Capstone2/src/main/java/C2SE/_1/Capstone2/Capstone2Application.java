fa9eab042c1385207ffe89470cb31dbd94af0932
ducthin <ducthinh123b@gmail.com>
2026-03-05 14:36:26 +0700

    first commit

---
+++ Backend/Capstone2/src/main/java/C2SE/_1/Capstone2/Capstone2Application.java
@@ -0,0 +1,13 @@
+package C2SE._1.Capstone2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Capstone2Application.class, args);
+	}
+
+}
---
+++ Backend/Capstone2/src/test/java/C2SE/_1/Capstone2/Capstone2ApplicationTests.java
@@ -0,0 +1,13 @@
+package C2SE._1.Capstone2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
